ae48adc3da3325496e747b18dae562b32f026844
Muby <ravaabeenaamuby@gmail.com>
2026-02-18 21:54:20 +0500

    Add products API

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  output: "server",
   adapter: cloudflare(),
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: { plugins: [tailwindcss()] },
 });
