587119da2b7ca76045df2c228aae4d41f5832648
Muby <ravaabeenaamuby@gmail.com>
2026-04-26 22:26:21 +0500

    contact update

---
+++ src/lib/contact.ts
@@ -0,0 +1,8 @@
+export const CONTACT = {
+  email: "hello@darkroom.mv",
+  phone: "+960 749 6670",
+  phoneTel: "+9607496670",
+  instagram: "https://www.instagram.com/darkroom.bysmolbo1/",
+  instagramHandle: "@darkroom.bysmolbo1",
+  telegram: "https://t.me/+9607496670",
+};
