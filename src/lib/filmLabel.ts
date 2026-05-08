94dcf3673107e66a7b8f12f953c0a4cb0e434678
Muby <ravaabeenaamuby@gmail.com>
2026-05-08 21:23:52 +0500

    hero carousel

---
+++ src/lib/filmLabel.ts
@@ -0,0 +1,36 @@
+const STOCKS: [RegExp, string][] = [
+  [/^ektar\s*100/i,       'Kodak Ektar 100'],
+  [/^gold\s*200/i,        'Kodak Gold 200'],
+  [/^gold\s*100/i,        'Kodak Gold 100'],
+  [/^portra\s*800/i,      'Kodak Portra 800'],
+  [/^portra\s*400/i,      'Kodak Portra 400'],
+  [/^portra\s*160/i,      'Kodak Portra 160'],
+  [/^ultramax\s*400/i,    'Kodak UltraMax 400'],
+  [/^colorplus\s*200/i,   'Kodak ColorPlus 200'],
+  [/^hp5/i,               'Ilford HP5 Plus'],
+  [/^fp4/i,               'Ilford FP4 Plus'],
+  [/^delta\s*3200/i,      'Ilford Delta 3200'],
+  [/^delta\s*400/i,       'Ilford Delta 400'],
+  [/^delta\s*100/i,       'Ilford Delta 100'],
+  [/^superia\s*x-tra/i,   'Fuji Superia X-TRA'],
+  [/^superia/i,           'Fuji Superia'],
+  [/^velvia\s*50/i,       'Fuji Velvia 50'],
+  [/^velvia\s*100/i,      'Fuji Velvia 100'],
+  [/^provia/i,            'Fuji Provia 100F'],
+  [/^pro400h/i,           'Fuji Pro 400H'],
+  [/^cinestill\s*800/i,   'CineStill 800T'],
+  [/^cinestill\s*50/i,    'CineStill 50D'],
+  [/^lomo\s*800/i,        'Lomography 800'],
+  [/^lomo\s*400/i,        'Lomography 400'],
+  [/^lomo\s*100/i,        'Lomography 100'],
+  [/^redscale/i,          'Lomography Redscale'],
+];
+
+export function filmLabel(key: string): string {
+  const filename = (key.split('/').pop() ?? key).replace(/(\.\w+)+$/, '');
+  const name = filename.split('_')[0];
+  for (const [re, label] of STOCKS) {
+    if (re.test(name)) return label;
+  }
+  return name.replace(/([A-Za-z])(\d{2,})/, '$1 $2');
+}
