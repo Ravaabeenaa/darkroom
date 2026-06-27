702e5aec99c723f2ab9f61cc7ec25431c34aea69
Muby <ravaabeenaamuby@gmail.com>
2026-06-27 12:10:49 +0500

    contact updated

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -1,7 +1,7 @@
 export const contact = {
   email: "info@darkroombysmolbo1.shop",
-  phone: "+960 749 6670",
-  telegram: "https://t.me/+9607496670",
+  phone: "+960 966 5667",
+  telegram: "https://t.me/+9609665667",
   instagram: "https://www.instagram.com/darkroom.bysmolbo1/",
   instagram_handle: "@darkroom.bysmolbo1",
 } as const;
